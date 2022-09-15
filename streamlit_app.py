c253bcde740a7dcc2fa92120995a21120813548e
bpjena <binay.bapu@gmail.com>
2022-09-14 19:38:05 -0700

    Update streamlit_app.py
    add pandas csv read

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('BPJ New Healthy Diner')
 streamlit.header('Breakfast Favorites')
@@ -8,3 +9,5 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)

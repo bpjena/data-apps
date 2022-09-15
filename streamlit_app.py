0349be8a838ea508885af07c34477666e57b1d31
bpjena <binay.bapu@gmail.com>
2022-09-14 19:27:05 -0700

    Update streamlit_app.py
    update icons on text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
-streamlit.title('My New Business Idea')
+streamlit.title('BPJ New Healthy Diner')
+
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')

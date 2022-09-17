e46ca33cd619943f219dcdbdf22655e078d71f66
bpjena <binay.bapu@gmail.com>
2022-09-16 21:40:45 -0700

    Update streamlit_app.py
    display fruits per index select

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,9 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
-streamlit.dataframe(my_fruit_list)
+# streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)

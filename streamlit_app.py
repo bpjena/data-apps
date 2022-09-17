21a0062e508b75eee91a982f96c8840e15e48097
bpjena <binay.bapu@gmail.com>
2022-09-17 00:42:33 -0700

    Update streamlit_app.py
    add header for request section

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 # New Section to display fruityvice api response
+streamlit.header("Fruityvice Fruit Advice!")
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)

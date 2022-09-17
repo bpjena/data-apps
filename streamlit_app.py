64f7f6745934ba07121384cdbc0e005672ccafd2
bpjena <binay.bapu@gmail.com>
2022-09-17 00:49:37 -0700

    Update streamlit_app.py
    add text entry box

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,3 +30,6 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # convert to tabular format
 streamlit.dataframe(fruityvice_normalized)
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)

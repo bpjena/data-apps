e8c3e3bb6e3e327706f43f8dc25e9b64a0584bb8
bpjena <binay.bapu@gmail.com>
2022-09-17 00:44:08 -0700

    Update streamlit_app.py
    add json datatype

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,4 +25,4 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())

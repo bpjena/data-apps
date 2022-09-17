c8fa2ebef68aec251b3205fa18c69fab8721566c
bpjena <binay.bapu@gmail.com>
2022-09-17 12:44:07 -0700

    Update streamlit_app.py
    add connector code

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,3 +34,11 @@ streamlit.dataframe(fruityvice_normalized)
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
+
+# Section for Snowflake connector ops
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)

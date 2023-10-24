c138cbd8b1fecb2426e80add70bf6ba51df07e31
Binay Prasanna Jena <binay.bapu@gmail.com>
2023-10-24 09:30:28 +0530

    snowflake + sql queries

---
+++ snowflake_real_estate/streamlit/BR_residential_properties.py
@@ -0,0 +1,4 @@
+# ref: https://developers.snowflake.com/demos/interactive-data-analysis-streamlit/
+# data-source: https://github.com/OviedoVR/Brazilian-properties-appreciation/blob/main/data/BR_real_estate_appreciation_Q1_2023.csv
+
+
